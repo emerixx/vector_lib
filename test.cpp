196b5817122b64a71a70f20017877bb01195e70b
emerixx <juraj.svatek@gmail.com>
2024-09-16 04:04:42 +0200

    backup b4 sleep

--- test.cpp
+++ test.cpp
@@ -26,10 +26,10 @@ int main() {
   while (!WindowShouldClose()) {
     if (theta < 2 * pi) {
       theta += 1.f / 600;
-      // mtrx[0] = {cos(theta), -sin(theta)};
-      mtrx[0] = {1, theta};
-      // mtrx[1] = {sin(theta), cos(theta)};
-      mtrx[1] = {theta, 1};
+      mtrx[0] = {cos(theta), -sin(theta)};
+      // mtrx[0] = {1, theta};
+      mtrx[1] = {sin(theta), cos(theta)};
+      // mtrx[1] = {theta, 1};
     } else {
       theta = 0;
     }
@@ -42,7 +42,7 @@ int main() {
 
     ClearBackground(BLACK);
     DrawTexture(grid, 0, 0, WHITE);
-    drawGrid(10, mtrx);
+    drawGrid(30, mtrx);
     // drawVector(origin, vec, clrs[0]);
     //  drawGrid(60, normal_matrix);
 
